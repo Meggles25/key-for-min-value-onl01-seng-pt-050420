5e22d7cdada57e87ed6ee34434ea2ce3152c7310
Megan Gomes <megan.gomes25@gmail.com>
2020-05-14 22:11:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,9 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |key, value|
-        if min_num < value
-          return key
-          end
-        end
+    if min_num < value
+      return key
+      end
+    end
   end
 end
