f083b027e7450ca8b15457f9b14622257272a125
Megan Gomes <megan.gomes25@gmail.com>
2020-05-14 22:17:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,11 @@
 
 def key_for_min_value(name_hash)
   lowest_value = nil
-  
-  name_hash.each do |key, value|
-    if min_num < value
-      return key
+  return_key = nil
+    name_hash.each do |key, value|
+      if min_num < value
+        return key
+        end
       end
     end
   end
